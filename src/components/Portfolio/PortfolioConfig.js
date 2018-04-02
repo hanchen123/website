731276e4acaa507ecbd9b91b8bf7387e1a182157
hanchen123 <chenhan728@gmail.com>
2018-04-01 20:20:23 -0400

    add more

--- src/components/Portfolio/PortfolioConfig.js
+++ src/components/Portfolio/PortfolioConfig.js
@@ -1,18 +1,43 @@
 import portfolio0 from "../../assets/portfolio0.jpg";
+import portfolio1 from "../../assets/portfolio1.jpg";
+import portfolio2 from "../../assets/portfolio2.jpg";
+import portfolio3 from "../../assets/portfolio3.jpg";
+import portfolio4 from "../../assets/portfolio4.jpg";
 import logo0 from "../../assets/logo0.png";
 
 export default [{
   image: portfolio0,
   logo: logo0,
-  headline: "Samsung Galaxy S9/S9+",
-  subHeadline: "Custom configurator for purchase your Galaxy S9/S9+",
-  link: "/portfolio/mobile-configurator",
+  headline: "Samsung Galaxy Products",
+  subHeadline: "Custom configurator for purchase your Samsung Galaxy Products",
+  link: "/portfolio/galaxy-products-configurator",
   refs: ["slideRight0", "slideLeft1"]
 }, {
-  image: portfolio0,
+  image: portfolio1,
   logo: logo0,
-  headline: "Samsung Galaxy S9/S9+",
-  subHeadline: "Custom configurator for purchase your Galaxy S9/S9+",
-  link: "/portfolio/mobile-configurator",
+  headline: "Samsung TVs",
+  subHeadline: "Custom configurator for purchase your Samsung TVs",
+  link: "/portfolio/tvs-configurator",
   refs: ["slideLeft2", "slideRight3"]
+}, {
+  image: portfolio2,
+  logo: logo0,
+  headline: "Samsung Home Appliances",
+  subHeadline: "Custom configurator for purchase your Home Appliances",
+  link: "/portfolio/home-appliances-configurator",
+  refs: ["slideLeft4", "slideRight5"]
+}, {
+  image: portfolio3,
+  logo: logo0,
+  headline: "Samsung Product Finder",
+  subHeadline: "Product Finder guides your customers to the right products",
+  link: "/portfolio/product-finder",
+  refs: ["slideLeft6", "slideRight7"]
+}, {
+  image: portfolio4,
+  logo: logo0,
+  headline: "Samsung US Site",
+  subHeadline: "Replatform Samsung.com/us using a modular, template approach based on Adobe CMS",
+  link: "/portfolio/samsung-us-site",
+  refs: ["slideLeft8", "slideRight9"]
 }];
