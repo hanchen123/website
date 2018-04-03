ffaab120dde10d65f7be79c77f8e27bb46efa16d
hanchen123 <chenhan728@gmail.com>
2018-04-02 22:38:29 -0400

    init SubPortfolio

---
+++ src/components/SubPortfolio/Config.js
@@ -0,0 +1,20 @@
+import desktop0 from "../../assets/desktop0.jpg";
+import mobile0 from "../../assets/mobile0.jpg";
+
+export default {
+  "galaxy-products-configurator": {
+    headline: "Samsung Galaxy Products",
+    subHeadline: "Custom configurator for purchase your Samsung Galaxy Products",
+    description: `the galaxy products configurator is an interactive page where the user will finish their purchase of  
+      galaxy products based on his selections of several steps. It's currently used for galaxy s8/s9/note8 purchase.
+      <br><br>I lead a front-end group of 3 people to develop this project from ground up, and delivered it into production 
+      within 3 weeks.<br><br>This project is built on React/Redux in combination with Sass, and page transition is handled by 
+      history API so each step can be cached and deeplinked in browser. It's full responsive and well tagged for marketing 
+      purpose. All the data is retrieved by RESTful API.`,
+    link: "https://www.samsung.com/us/smartphones/galaxy-s9/buy",
+    release: "February 2018",
+    tech: ["ReactJS", "Redux", "ES6", "Sass", "HTML5", "Webpack", "Restful APIs", "Full Responsive"],
+    desktop: desktop0,
+    mobile: mobile0
+  }
+};
--- src/components/SubPortfolio/SubPortfolio.js
+++ src/components/SubPortfolio/SubPortfolio.js
@@ -1,5 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Config from "./Config";
+import styles from "./SubPortfolio.scss";
 
 class SubPortfolio extends React.PureComponent {
   render() {
@@ -7,8 +9,34 @@ class SubPortfolio extends React.PureComponent {
       match
     } = this.props;
 
+    const content = Config[match.params.id];
+    const tech = content.tech.map((val, idx) => {
+      return(<li key={idx}>{val}</li>);
+    });
+
     return (
-      <div>SubPortfolio: {match.params.id}</div>
+      <section className={styles.wrapper}>
+        <div className={styles.innerWrapper}>
+          <h1 className={styles.h1}>{content.headline}</h1>
+          <div className={styles.intro}>
+            <h2>{content.subHeadline}</h2>
+            <p dangerouslySetInnerHTML={{__html: content.description}}></p>
+            <a className={styles.link} href={content.link} target="_blank" rel="noopener noreferrer">SEE IT LIVE</a>
+          </div>
+          <div className={styles.screen}>
+            <img className={styles.desktopImg} src={`/${content.desktop}`} alt="desktop-view" />
+            <img className={styles.mobileImg} src={`/${content.mobile}`} alt="mobile-view" />
+          </div>
+          <div className={styles.clear}>
+            <h3 className={styles.h3}>Released</h3>
+            <p>{content.release}</p>
+          </div>
+          <div className={styles.clear}>
+            <h3 className={styles.h3}>Technologies</h3>
+            <ul>{tech}</ul>
+          </div>
+        </div>  
+      </section>    
     );
   }
 }
